806569608df48204824f6b2b722add8c939d3652
wglm2014 <glinaresmiranda@yahoo.com>
2019-07-08 12:50:53 -0400

    links added to index.html and routes to htmlRoutes, background to registration forms, loremipsum changed to real pragraphs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,4 +3,14 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => { res.sendFile("index.html") });
 
+router.get("/farmer-register", (req, re) => {
+    res.sendFile("farmerRegister.html");
+});
+router.get("/shopper-register", (req, re) => {
+    res.sendFile("shopperRegister.html");
+});
+router.get("/customer-register", (req, re) => {
+    res.sendFile("customerRegister.html");
+});
+
 module.exports = router;
